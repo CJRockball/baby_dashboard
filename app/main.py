4f3c270a078156196837476d72d5945ce17724af
Patrick Carlberg <11623836+CJRockball@users.noreply.github.com>
2022-01-14 13:49:32 +0800

    changed requirements

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
-import json
 import pathlib
-from typing import Optional
 
-import requests
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
